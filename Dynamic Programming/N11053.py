3ac147abe81a2cee628402adbb62860c4d588d52
Shin Changyu <scv1702@naver.com>
2022-03-31 07:05:54 +0000

    ✅ Solve N11053

--- Dynamic Programming/N11053.py
+++ Dynamic Programming/N11053.py
@@ -1,66 +1,24 @@
 import sys
 
+# LIS: Longest Increasing Subsequence
 def solve(sequence, n):
-    dp_table = [0] * 1000
+    dp_table = [0] * n
 
+    # LIS의 길이를 저장한다.
     dp_table[0] = 1
-    max_term = sequence[0]
 
-    # A = {10, 20, 10, 30, 20, 50}
-    '''
-    1 10
-    2 20
-    2 20
-    3 30
-    3 30
-    4 50
-
-
-    '''
-
-
-    '''
-    10
-    10 20
-    10 20
-    10 20 30
-    10 20 30
-    10 20 30 50
-    '''
-
-    # A = {6, 7, 1, 2, 3, 4, 5}
-    '''
-    6
-    6 7
-    6 7
-    6 7 / 1 2
-    1 2 3
-    1 2 3 4
-    1 2 3 4 5
-    '''
-
-    '''
-    1 6
-    2 7
-    2 7
-    2 7 / 2 2
-    3 3
-    4 4 
-    5 5
-
-    
-    '''
-
-    # 1. sequence[i]가 가장 긴 증가하는 부분 수열에 속하는 경우 -> sequence[i]가 가장 긴 증가하는 부분 수열의 최대항보다 큰 경우 
-    # 2. sequence[i]가 가장 긴 증가하는 부분 수열에 속하지 않는 경우 -> sequence[i]가 가장 긴 증가하는 부분 수열의 최대항보다 크지 않은 경우
-
+    # sequence에는 LIS에서 max num이 될 수가 무조건 하나 존재한다.
+    # 그렇다면, sequence[i]가 LIS의 max num이 된다고 가정하자.
+    # sequence[i]보다 작은 수들로 만들 수 있는 LIS의 길이에 +1 한 것이 현재 LIS의 길이이다.
     for i in range(1, n):
-        
-        dp_table[i] = dp_table[i-1] + 1
-        
+        # sequence[i]보다 작은 수가 없다면, LIS의 길이는 1
+        dp_table[i] = 1
+        for j in range(0, i):
+            if sequence[j] < sequence[i]:
+                dp_table[i] = max(dp_table[i], dp_table[j] + 1)
 
-        
-    
+    return max(dp_table)
+
 N = int(sys.stdin.readline().rstrip())
 
 sequence = [0] * 1000
